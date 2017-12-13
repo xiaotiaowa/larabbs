566233087f8d5c7b8d5b22483801e4b10e00fe5f
xiaotiaowa <1024073648@qq.com>
2017-12-13 23:27:05 +0000

    Finish user auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@
 // });
 
 Route::get('/', 'PagesController@root')->name('root');
+Auth::routes();
+//Authentication Rotes
+// Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+// Route::post('login', 'Auth\LoginController@login');
+// Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+//
+// //Registration Routes...
+// Route::get('register', 'Auth\RegisterController@showRegistrationFom')->name('register');
+// Route::post('register', 'Auth\RegisterController@register');
+//
+// // Password Reset Routes...
+// Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+// Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+// Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+// Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+// Route::get('/', 'HomeController@index')->name('home');
