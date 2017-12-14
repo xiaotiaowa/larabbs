ddb9d48c44c575661501b9638db22f0608fe31cb
xiaotiaowa <1024073648@qq.com>
2017-12-14 19:31:13 +0000

    Prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -22,18 +22,10 @@ use App\Models\Topic;
 
 class TopicObserver
 {
-    public function creating(Topic $topic)
-    {
-        //
-    }
-
-    public function updating(Topic $topic)
-    {
-        //
-    }
 
     public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body, 'user_topic_body');
         // make_excerpt() 是我们自定义的辅助方法，我们需要在 helpers.php 文件中添加：
        $topic->excerpt = make_excerpt($topic->body);
     }
